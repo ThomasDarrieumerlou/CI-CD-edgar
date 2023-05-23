542f3bf94d28554040e243d0299fc1be5a486d12
ThomasDarrieumerlou <72026268+ThomasDarrieumerlou@users.noreply.github.com>
2023-05-23 13:09:48 +0200

    Update test.hs

--- test.hs
+++ test.hs
@@ -1,8 +1,2 @@
-import Network.HTTP.Conduit
-import Control.Monad.IO.Class (liftIO)
-
-main = withManager $ \manager -> do
-    request <- parseUrl "http://www.winsoft.sk"
-    liftIO $ print request
-    response <- httpLbs request manager
-    liftIO $ print response
+main :: IO ()
+main = putStrLn "Hello World"
